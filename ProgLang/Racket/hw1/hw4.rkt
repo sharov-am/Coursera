72ff49a61560b46db3dd16ed4fb2429d61881295
Anton Sharov <sharov_am@mail.ru>
2013-02-19 22:09:03 +0400

    hw3 work commit

--- ProgLang/Racket/hw1/hw4.rkt
+++ ProgLang/Racket/hw1/hw4.rkt
@@ -15,3 +15,21 @@
 (define (string-append-map xs suffix)
   (map (lambda(str)(string-append str suffix)) xs))
 
+
+(define (list-nth-mod xs n)
+   (cond [(> 0 n) error "list-nth-mod: negative number"]
+         [(null? xs) error "list-nth-mod: empty list"]
+         [#t (let ([rem  (remainder n  (length xs))])
+                (car (list-tail xs rem)))]))
+         
+;         
+;
+;(define (test_zero n)
+;  (cond [(= 0 n) "zero"]
+;        [#t "non zero"]))
+;
+;(define (silly_triple x1);x = 2
+;  (letrec ([x (+ x1 3)];x = 5
+;           [y (+ x 2)];y = 7
+;           [z (+ y 2)]);z = 9
+;    (+ x y z -5)))
